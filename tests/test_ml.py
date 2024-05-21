ca9a50f15eff98c2b5653bdd7ba0cb99a7ef5fdc
Kuba Kolodziejczyk <kuba@conceptual.jp>
2024-05-21 22:51:26 +0900

    Added generator test
    A simple test to check that shape of output is the same as shape of input

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -2,13 +2,18 @@
 Dummy test file
 """
 
+import numpy as np
 
-def test_foo():
+import net.ml
+
+
+def test_cyclegan_generator():
     """
-    Simple dummy test
+    Test cyclegan generator create data of the same shape as input data
     """
 
-    a = 1
-    b = 1
+    input_data = np.ones((4, 256, 256, 3))
 
-    assert a == b
+    actual = net.ml.GeneratorBuilder().get_model().predict(input_data, verbose=0)
+
+    assert actual.shape == input_data.shape
